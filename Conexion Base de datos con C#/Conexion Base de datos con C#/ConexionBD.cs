3d923c0bcf93d6ed8ea61b96bf5c5240a9b68586
Fredy Esteban <fredy.esteban25@hotmail.com>
2024-01-29 23:45:11 -0500

    Backup

---
+++ Conexion Base de datos con C#/Conexion Base de datos con C#/ConexionBD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Npgsql; //Se añade libreria
+
+namespace Conexion_Base_de_datos_con_C_
+{
+    internal class ConexionBD
+    {
+        private NpgsqlConnection conn;
+
+
+        public ConexionBD() //Conexion con base de datos
+        {
+            conn = new NpgsqlConnection("Server = localhost; Port =5432; User Id = postgres; Password =250436; Database = Ejemplo1");
+        }
+
+        public NpgsqlConnection Abrir_Conexion()
+        {
+            try // Se utiliza try y catch por si hay algun error de conexion
+            {
+                conn.Open(); //Abrir conexion
+                Console.WriteLine("Se realizo conexión...");
+                return conn;
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Error de conexión");
+                return null; //en dado caso que no funcione
+            }
+        }
+
+        public void Cerrar_Conexion()
+        { conn.Close(); }
+
+    }
+}
---
+++ Conexion Base de datos con C#/Conexion Base de datos con C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using Conexion_Base_de_datos_con_C_;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using Npgsql;// Se instala Npgsql en paquetes NuGet
+
+namespace Interfaz
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ConexionBD connectbd = new ConexionBD();
+            string query = "insert into public.usuarios (nombre) values ('Fredy')"; //Comando de base de datos, dato a ingresar
+
+            NpgsqlCommand cmd = new NpgsqlCommand(query, connectbd.Abrir_Conexion()); //Abrir conexion
+
+            //Insersion de datos
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Dato enviado");
+
+            //Cerrar conexion despues de actualizar base de datos
+            connectbd.Cerrar_Conexion();
+        }
+
+    }
+}
